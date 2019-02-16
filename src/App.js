333e8919e6845db09b9415fe344ef87037bbf693
root <root@localhost.localdomain>
2019-02-17 05:50:35 +0900

    Content finished

--- src/App.js
+++ src/App.js
@@ -30,56 +30,38 @@ class App extends Component {
             {belavia: 'Белавия'}
         ],
         tickets_info: [
-            {direction: [
-                {paris_london: 'Paris-London'},
-                {moscow_berlin: 'Moscow-Berlin'},
-                {bishkek_moscow: 'Bishkek-Moscow'},
-                {pekin_moscow: 'Pekin-Moscow'},
-                {almaty_bishkek: 'Almaty-Bishkek'},
-                {newyork_washington: 'New York-Washington'}
-                ]},
-            {number: [
-                {paris_london: '№25'},
-                {moscow_berlin: '№13'},
-                {bishkek_moscow: '№5'},
-                {pekin_moscow: '№9'},
-                {almaty_bishkek: '№12'},
-                {newyork_washington: '№21'}
-                ]},
-            {price: [
-                {paris_london: '1200'},
-                {moscow_berlin: '5000'},
-                {bishkek_moscow: '900'},
-                {pekin_moscow: '1100'},
-                {almaty_bishkek: '500'},
-                {newyork_washington: '2000'}                
-                ]},
-            {date: [
-                {paris_london: '12.03.2019'},
-                {moscow_berlin: '20.02.2019'},
-                {bishkek_moscow: '10.03.2019'},
-                {pekin_moscow: '30.06.2020'},
-                {almaty_bishkek: '04.03.2019'},
-                {newyork_washington: '02.04.2019'}  
-                ]},
-            {buy: [
-                    {buy: 'Buy ticket'},
-                    {href: '#'}
-                ]}
+                {way: 'Paris-London', num: '№25', price: '13000', date: '20.02.2019'},
+                {way: 'Moscow-Berlin', num: '№13', price: '15000', date: '18.02.2019'},
+                {way: 'Bishkek-Moscow', num: '№3', price: '1300', date: '28.02.2019'},
+                {way: 'Pekin-Moscow', num: '№19', price: '1000', date: '20.05.2019'},
+                {way: 'Almaty-Bishkek', num: '№16', price: '3300', date: '25.02.2019'},
+                {way: 'New York-Washington', num: '№19', price: '21000', date: '30.06.2019'},
+                {buy: 'Buy ticket', href: '#'}
         ]
     }
+
     render() {
         return (
             <div className="App">
-                <Header logo_src={this.state.navbar[0].logo_src} logo_alt={this.state.navbar[1].logo_alter} logo_text={this.state.navbar[2].logo_text} 
+                <Header 
+                    logo_src={this.state.navbar[0].logo_src} logo_alt={this.state.navbar[1].logo_alter} logo_text={this.state.navbar[2].logo_text} 
                     countries={this.state.nav_links[0].countries} airplanes={this.state.nav_links[1].airplanes} flights={this.state.nav_links[2].flights} 
                     login={this.state.nav_links[3].login} registration={this.state.nav_links[4].registration} />
-                <Sidebar c_1={this.state.airline_list[0].new_zealand} c_2={this.state.airline_list[1].atlantic} c_3={this.state.airline_list[2].aurigny} 
+
+                <Sidebar 
+                    c_1={this.state.airline_list[0].new_zealand} c_2={this.state.airline_list[1].atlantic} c_3={this.state.airline_list[2].aurigny} 
                     c_4={this.state.airline_list[3].islands} c_5={this.state.airline_list[4].flybe} c_6={this.state.airline_list[5].baltic} 
-                    c_7={this.state.airline_list[6].bulgaria} c_8={this.state.airline_list[7].austrian} c_9={this.state.airline_list[8].belavia} />
-                <Content way_ticket_1={this.state.tickets_info[0][0].paris_london} number_ticket_1={this.state.tickets_info[2][0].paris_london} 
-                    price_ticket_1={this.state.tickets_info[2][0].paris_london} date_ticket_1={this.state.tickets_info[3][0].paris_london} 
-                    buy={this.state.tickets_info[4][0].buy} buy_href={this.state.tickets_info[4][0].href} />
+                    c_7={this.state.airline_list[6].bulgaria} c_8={this.state.airline_list[7].austrian} c_9={this.state.airline_list[8].belavia} 
+                    href={this.state.navbar[0].href} />
+                    
+                <Content 
+                    way_ticket_1={this.state.tickets_info[0].way} num_ticket_1={this.state.tickets_info[0].num} price_ticket_1={this.state.tickets_info[0].price} date_ticket_1={this.state.tickets_info[0].date}
+                    way_ticket_2={this.state.tickets_info[1].way} num_ticket_2={this.state.tickets_info[1].num} price_ticket_2={this.state.tickets_info[1].price} date_ticket_2={this.state.tickets_info[1].date}
+                    way_ticket_3={this.state.tickets_info[2].way} num_ticket_3={this.state.tickets_info[2].num} price_ticket_3={this.state.tickets_info[2].price} date_ticket_3={this.state.tickets_info[2].date}
+                    way_ticket_4={this.state.tickets_info[3].way} num_ticket_4={this.state.tickets_info[3].num} price_ticket_4={this.state.tickets_info[3].price} date_ticket_4={this.state.tickets_info[3].date}
+                    way_ticket_5={this.state.tickets_info[4].way} num_ticket_5={this.state.tickets_info[4].num} price_ticket_5={this.state.tickets_info[4].price} date_ticket_5={this.state.tickets_info[4].date}
+                    way_ticket_6={this.state.tickets_info[5].way} num_ticket_6={this.state.tickets_info[5].num} price_ticket_6={this.state.tickets_info[5].price} date_ticket_6={this.state.tickets_info[5].date} 
+                    buy={this.state.tickets_info[6].buy} href={this.state.tickets_info[6].href} />
             </div>
         );
     }
--- src/hw52/airline.js
+++ src/hw52/airline.js
@@ -3,23 +3,23 @@ import React from 'react';
 const Header = props => {
     return (
         <div className="header">
-        	<a href='#'>
+        	<a href='props.href'>
         		<img src={props.logo_src} alt='props.logo_alter' className='logo' />
         		<h5 className='logo_text'>{props.logo_text}</h5>
         	</a>
         	<ul className='nav'>
     			<li>
-        			<a href='#'>
+        			<a href='props.href'>
         				{props.countries}
         			</a>
     			</li>
     			<li>
-        			<a href='#'>
+        			<a href='props.href'>
         				{props.airplanes}
         			</a>
     			</li>
     			<li>
-        			<a href='#'>
+        			<a href='props.href'>
         				{props.flights}
         			</a>
     			</li>
@@ -40,50 +40,115 @@ const Sidebar = props => {
     return (
     <div className="sidebar">
         	<ul className='sidebar_list'>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_1}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_2}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_3}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_4}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_5}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_6}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_7}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_8}
     				</li>
     			</a>
-        		<a href='#'>
+        		<a href='props.href'>
         			<li>
         				{props.c_9}
         			</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_6}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_7}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_8}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_9}
+        			</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_6}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_7}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_8}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_9}
+        			</li>
+    			</a>
+    			<a href='props.href'>
+        			<li>
+        				{props.c_1}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_2}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_3}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_4}
+    				</li>
+    			</a>
+        		<a href='props.href'>
+        			<li>
+        				{props.c_5}
+    				</li>
     			</a>
         	</ul>
         </div>
@@ -93,21 +158,70 @@ const Sidebar = props => {
 const Content = props => {
     return (
         <div className="content">
-        	<ul className='flight_list'>
-        		<li>
-        			<h5>
-        				{props.way_ticket_1}
-        			</h5>
-        			<p>
-        				Number of flight: {props.number_ticket_1}
-        				Price: {props.price_ticket_1}
-        				Date: {props.date_ticket_1}
-        			</p>
-        			<a href='{props.buy_href}'>
-        				{props.buy}
-        			</a>
-        		</li>
-        	</ul>
+        	<div className='card_list'>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_1}</h3>
+		        	<h4>Ticket {props.num_ticket_1}</h4>
+		        	<h4>Price: {props.price_ticket_1}</h4>
+		        	<h4>Date: {props.date_ticket_1}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_2}</h3>
+		        	<h4>Ticket {props.num_ticket_2}</h4>
+		        	<h4>Price: {props.price_ticket_2}</h4>
+		        	<h4>Date: {props.date_ticket_2}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_3}</h3>
+		        	<h4>Ticket {props.num_ticket_3}</h4>
+		        	<h4>Price: {props.price_ticket_3}</h4>
+		        	<h4>Date: {props.date_ticket_3}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_4}</h3>
+		        	<h4>Ticket {props.num_ticket_4}</h4>
+		        	<h4>Price: {props.price_ticket_4}</h4>
+		        	<h4>Date: {props.date_ticket_4}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_5}</h3>
+		        	<h4>Ticket {props.num_ticket_5}</h4>
+		        	<h4>Price: {props.price_ticket_5}</h4>
+		        	<h4>Date: {props.date_ticket_5}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+	        	<div className='ticket_card'>
+		        	<h3>Way: {props.way_ticket_6}</h3>
+		        	<h4>Ticket {props.num_ticket_6}</h4>
+		        	<h4>Price: {props.price_ticket_6}</h4>
+		        	<h4>Date: {props.date_ticket_6}</h4>
+		        	<a href={props.href} className='buy_button'>
+		        		{props.buy}
+		        	</a>
+		        </div>
+		    </div>
+        </div>
+    );
+};
+
+const Footer = props => {
+    return (
+        <div className="footer">
+        	
         </div>
     );
 };
